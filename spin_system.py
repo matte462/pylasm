09e2ad463c366793927239b92de8f9095eacf2f7
Matteo Costa <mattecosta48@gmail.com>
2024-09-28 01:18:14 +0200

    New implementation of spin-spin correlations and magnetization

--- spin_system.py
+++ spin_system.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import ishermitian
 
 class SpinSystem() :
     '''
@@ -237,7 +238,7 @@ class SpinSystem() :
                 S_z[a-1][b-1] = complex((spin+1-a)*(a==b))
         return np.array([S_x,S_y,S_z])
 
-    def build_hamiltonian(self,J_couplings: list,NN_vectors: list,max_NN_shell: int,shell_digits: int,n_dim: int) -> 'np.ndarray' :
+    def build_hamiltonian(self,J_couplings: list,NN_vectors: list,max_NN_shell: int,shell_digits: int,B_field: 'np.ndarray',n_dim: int,tol_imag: float) -> 'np.ndarray' :
         '''
         Returns the Hamiltonian matrix for the interacting spin system.
         As mentioned below, the order of the elements in the provided arrays is important,
@@ -248,10 +249,13 @@ class SpinSystem() :
             NN_vectors (list): Ordered set of 3D NN vectors;
             max_NN_shell (int): The highest NN shell to be considered when computing the spin-spin interactions;
             shell_digits (int): Number of digits to be considered during the identification of the NN shells by distance;
-            n_dim (int): Number of spatial dimensions of the spin system under study. 
+            B_field (np.ndarray): 3D vector for the external homogeneous magnetic field;
+            n_dim (int): Number of spatial dimensions of the spin system under study;
+            tol_imag (float): Tolerance on the imaginary part of Hamiltonian matrix elements.
         '''
         Nspins = self.get_Nspins()
         spin_mult = self.get_spin_mult()
+        S_vec = self.build_spin_operator()
 
         # Initialize the Spin Hamiltonian matrix
         dim = spin_mult**Nspins
@@ -268,7 +272,10 @@ class SpinSystem() :
 
                 # Inner cycle over the second spins
                 for j in range(len(ith_NN_spins)) :
-                    if ith_NN_spins[j]<i : continue
+                    if ith_NN_spins[j]<i : 
+                        continue
+                    elif ith_NN_spins[j]==i :
+                        raise ValueError(f'The {nn}° NN shell for spin {i} includes some/all of its replica. Consider decreasing the max_NN_shell value or taking a larger unit cell.')
                         
                     # Compute the effective J tensor between the two spins
                     J_eff = self.compute_J_eff(J_couplings[nn-1],NN_vectors[nn-1],ith_NN_vecs[j],shell_digits)
@@ -276,6 +283,14 @@ class SpinSystem() :
                     # Compute the interaction term and update the Spin Hamiltonian
                     interaction_term = self.compute_pair_interaction(i,ith_NN_spins[j],J_eff)
                     H += interaction_term
+        
+        for i in range(Nspins) :
+            BS_product = B_field[0]*S_vec[0]+B_field[1]*S_vec[1]+B_field[2]*S_vec[2]
+            H += np.kron(np.kron(np.eye(spin_mult**i),BS_product),np.eye(spin_mult**(Nspins-i-1)))
+        
+        if not ishermitian(H,atol=tol_imag) :
+            raise ValueError('The Spin Hamiltonian matrix is not Hermitian.')
+        
         return H
     
     def compute_J_eff(self,J_couplings: list,NN_vectors: list,vector: 'np.ndarray',shell_digits: int) -> 'np.ndarray' :
@@ -326,84 +341,164 @@ class SpinSystem() :
                     aux_size = spin_mult**(second-first-1)
                     interaction_term += J_eff[a][b] * np.kron(np.kron(S_vec[a],np.eye(aux_size)), S_vec[b])
                 else :
-                    interaction_term += J_eff[a][b] * np.matmul(S_vec[a], S_vec[b])
+                    raise ValueError(f'The interaction term between spins {first} and {second} is not allowed.')
 
         # Adjust the shape by the proper tensor products  
         aux_size = spin_mult**(Nspins-second-1)
         final_term = np.kron(np.kron(np.eye(spin_mult**first), interaction_term), np.eye(aux_size))
         return final_term
     
-    def compute_spin_exp_value(self,state: 'np.ndarray',ref_spin: int) -> 'np.ndarray' :
+    def compute_spin_correlation(self,states: 'np.ndarray',GS_deg: int,first: int,second: int) -> 'np.ndarray' :
         '''
-        Returns the expectation value of the vector spin operator of the chosen magnetic site with respect
-        to the state passed as first argument.
+        Returns the spin-spin correlation between the two chosen magnetic sites. It is indeed obtained 
+        by calculating the expectation value of the scalar product of the associated spin operators.
+        Tensor products with identity matrices are also included in order to account for the order of
+        the two spins within the sequence.
         
         Args:
-            state (np.ndarray): Spinor state belonging to the Hilbert space of the composite spin system;
-            ref_spin (int): Index of the interested magnetic site. 
-        '''
-        Nspins = self.get_Nspins()
-        spin_mult = self.get_spin_mult()
-        S_vec = self.build_spin_operator()
-        
-        # Initialize the spin expectation value
-        exp_value = np.zeros(3)
-        
-        # Loop over all the spatial coordinates
-        for a in range(3) :
-            Sa_extended = np.kron(np.kron(np.eye(spin_mult**ref_spin), S_vec[a]), np.eye(spin_mult**(Nspins-ref_spin-1)))
-            Sa_expected = np.dot(np.matmul(state.conj(),Sa_extended),state.T)[0][0]
-            exp_value[a] = np.real(Sa_expected)
-        return exp_value
-    
-    def compute_magnetization(self,state: 'np.ndarray') -> 'np.ndarray' :
-        '''
-        Returns the magnetization vector of the composite spin system as proportional to the sum
-        of the spin expectation values associated with all the available magnetic sites. 
-        
-        Args:
-            state (np.ndarray): Spinor state belonging to the Hilbert space of the composite spin system.
-        '''
-        Nspins = self.get_Nspins()
-        M_vec = np.zeros(3)
-        
-        # Loop over all spin indices
-        for spin in range(Nspins) :
-            M_vec += (1.0/Nspins)*self.compute_spin_exp_value(state,spin)
-            
-        return M_vec
-    
-    def compute_spin_correlation(self,state: 'np.ndarray',first: int,second: int) -> 'np.ndarray' :
-        '''
-        Returns the spin-spin correlation matrix between the two chosen magnetic sites. Each element
-        is labelled by the components of the two spin operators in question and it is thus obtained 
-        by calculating the expectation value of their product.
-        Tensor products with identity matrices are also included in order to keep the order of the two
-        spins within the sequence.
-        
-        Args:
-            state (np.ndarray): Spinor state belonging to the Hilbert space of the composite spin system;
+            states (np.ndarray): Orthonormal column eigenstates of the spin Hamiltonian;
+            GS_deg (int): Ground-state degeneracy;
             first (int): Index of the first magnetic site;
             second (int): Index of the second magnetic site.
+        
+        Note:
+            It actually returns a 1D array of size 3 with the expectation values of the Sx*Sx, Sy*Sy and Sz*Sz
+            product operators. This may help the user to have the full picture of spin-spin relative orientation.
         '''
         Nspins = self.get_Nspins()
         spin_mult = self.get_spin_mult()
         S_vec = self.build_spin_operator()
+        states = states.T
         
-        # Initialize the spin-spin correlation matrix
-        corr_matrix = np.zeros((3,3))
+        # Target quantity
+        corr_vec = np.zeros(3)
+        
+        # Loop over all the spatial coordinates
+        for i in range(3) :
+            
+            # Initialize the spin-spin scalar product operator
+            aux_size = spin_mult**(abs(second-first)+1)
+            initial_product = np.zeros((aux_size,aux_size),dtype=complex)
+            if second!=first :
+                aux_size = spin_mult**(abs(second-first)-1)
+                initial_product = np.kron(np.kron(S_vec[i],np.eye(aux_size)), S_vec[i])
+            else :
+                initial_product = S_vec[i] @ S_vec[i]
+            
+            # Adjust its shape of the spin-spin scalar product operator
+            aux_size = spin_mult**(Nspins-max(first,second)-1)
+            spins_product = np.kron(np.kron(np.eye(spin_mult**min(first,second)),initial_product), np.eye(aux_size))
+            
+            # Compute its expectation value with respect to the given GS states
+            for n in range(GS_deg) :
+                corr_vec[i] += np.real(states[n].conj() @ (spins_product) @ states[n].T)/GS_deg
+            
+        return corr_vec
 
-        # Loop over all the spatial coordinates (twice)
-        for a in range(3) :
-            for b in range(3) :
-                aux_size1 = spin_mult**(second-first-1)
-                spin_product = np.kron(np.kron(S_vec[a],np.eye(aux_size1)), S_vec[b])
+    def compute_all_spin_correlations(self,states: 'np.ndarray',GS_deg: int) -> tuple :
+        '''
+        Returns all the inequivalent spin-spin correlation values of the system in question
+        by cycling over all the available spin pairs. 
+        No single-spin "self-correlation" is taken into account since that would
+        reduce to an expectation value of the squared spin operator (prop. to
+        identity), which is thus a mere constant (i.e. S*(S+1)).
+        
+        Args:
+            states (np.ndarray): Orthonormal column eigenstates of the spin Hamiltonian;
+            GS_deg (int): Ground-state degeneracy.
+        '''
+        Nspins = self.get_Nspins()
+        spin = self.get_spin()
+        
+        # Initialize the spin-spin correlation matrix and the contribution from Sx, Sy and Sz components
+        spin_corr_vals = spin*(spin+1)*np.eye(Nspins)
+        spin_corr_xs = np.zeros((Nspins,Nspins))
+        spin_corr_ys = np.zeros((Nspins,Nspins))
+        spin_corr_zs = np.zeros((Nspins,Nspins))
+        
+        # Cycle over all possible spin pairs
+        for n in range(Nspins) :
+            for m in range(n,Nspins) :
+                spin_corr_vec = self.compute_spin_correlation(states,GS_deg,n,m)
+                spin_corr = spin_corr_vec.sum()
+                
+                if n!=m :
+                    spin_corr_xs[n][m] = spin_corr_vec[0]
+                    spin_corr_xs[m][n] = spin_corr_vec[0]
+                    spin_corr_ys[n][m] = spin_corr_vec[1]
+                    spin_corr_ys[m][n] = spin_corr_vec[1]
+                    spin_corr_zs[n][m] = spin_corr_vec[2]
+                    spin_corr_zs[m][n] = spin_corr_vec[2]
+                    spin_corr_vals[n][m] = spin_corr
+                    spin_corr_vals[m][n] = spin_corr
+                else :
+                    spin_corr_xs[n][n] = spin_corr_vec[0]
+                    spin_corr_ys[n][n] = spin_corr_vec[1]
+                    spin_corr_zs[n][n] = spin_corr_vec[2]
+        
+        return spin_corr_xs, spin_corr_ys, spin_corr_zs, spin_corr_vals
 
-                # Adjust the shape by the proper tensor products  
-                aux_size2 = spin_mult**(Nspins-second-1)
-                final_product = np.kron(np.kron(np.eye(spin_mult**first), spin_product), np.eye(aux_size2))
-                
-                # Perform the products with the states
-                corr_matrix[a][b] = np.dot(np.matmul(state.conj(),final_product),state.T)[0][0]
-                
-        return corr_matrix
+    def compute_magnetization(self,states: 'np.ndarray',GS_deg: int,magn_output_mode: str) -> 'np.ndarray' :
+        '''
+        Returns the magnetization modulus of the composite spin system as proportional to the expectation value of
+        the sum of all the spin-spin scalar product operators. The definition of such scalar products depends on the chosen
+        string for the magn_output_mode variable. See example for clarity.
+        
+        Args:
+            states (np.ndarray): Orthonormal column eigenstates of the spin Hamiltonian;
+            GS_deg (int): Ground-state degeneracy;
+            magn_output_mode (str): String literal to select the definiton of the output magnetization.
+        
+        Example:
+            If magn_output_mode='M_z', then only the Sz-Sz correlation values are actually targeted
+            and thus the magnetization modulus operator is defined as
+            M = sqrt( sum_{i,j}( Sz_i @ Sz_j ) )
+            where Sz_i stands for the z component of the spin operator of the i-th magnetic site.
+            The cases for magn_output_mode='M_x' or 'M_y' are similar to the latter, except for taking
+            the x or y components of the spin operators respectively.
+            Finally, if magn_output_mode='M_full', all the spin-spin correlation vales are accessed,
+            leading to
+            M = sqrt( sum_{i,j}( S_i * S_j ) )
+            with S_i * S_j as the scalar product of the two spin operators in question.
+        '''
+        Nspins = self.get_Nspins()
+        spin_mult = self.get_spin_mult()
+        S_vec = self.build_spin_operator()
+        states = states.T
+        
+        # Target quantity
+        M2_operator = np.zeros((spin_mult**Nspins,spin_mult**Nspins),dtype=complex)
+        
+        # Choose which spin-spin correlations will contribute to the computation of the magnetization
+        selected_coords = {'M_x': [0],'M_y': [1],'M_z': [2],'M_full': [0,1,2]}
+        
+        # Loop over all the selected spatial coordinates
+        for i in selected_coords[magn_output_mode] :
+            
+            # Loop over all spin pairs (order is not important)
+            for n in range(Nspins) :
+                for m in range(n,Nspins) :
+            
+                    # Initialize the spin-spin scalar product operator
+                    aux_size = spin_mult**(m-n+1)
+                    initial_product = np.zeros((aux_size,aux_size),dtype=complex)
+                    if n!=m :
+                        aux_size = spin_mult**(m-n-1)
+                        initial_product = 2*np.kron(np.kron(S_vec[i],np.eye(aux_size)), S_vec[i])
+                    else :
+                        initial_product = S_vec[i] @ S_vec[i]
+            
+                    # Adjust its shape of the spin-spin scalar product operator
+                    aux_size = spin_mult**(Nspins-m-1)
+                    spins_product = np.kron(np.kron(np.eye(spin_mult**n),initial_product), np.eye(aux_size))
+                    
+                    # Add it to the squared magnetization operator
+                    M2_operator += spins_product
+        
+        # Compute its expectation value with respect to the given GS states
+        M2 = 0.0
+        for n in range(GS_deg) :
+            M2 += abs(np.real(states[n].conj() @ (M2_operator) @ states[n].T)/GS_deg)
+        M2 = np.sqrt(M2)/Nspins
+        
+        return M2
