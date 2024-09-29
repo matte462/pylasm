ccddd56205f9ee541f3dcee590cc6cc08b8d4422
Matteo Costa <mattecosta48@gmail.com>
2024-09-30 01:02:04 +0200

    New functions to save, plot and report data

--- global_functions.py
+++ global_functions.py
@@ -1,23 +1,40 @@
+from spin_system import SpinSystem
+from types import TracebackType
 import numpy as np
+import matplotlib.pyplot as plt
 import pylanczos as pl
+import traceback
+import json
 
 # Some useful global functions
+def log_exception(exc_type: type[BaseException],exc_value: BaseException,exc_traceback: TracebackType) -> None :
+    '''
+    Logs the details of raised Exceptions to a TXT file named SPIN_REPORT.txt.
+    
+    Args:
+        exc_type (type[BaseException): Type of the just raised Exception;
+        exc_value (BaseException): Instance of the just raised Exception;
+        exc_traceback (traceback.TracebackType): Traceback of the just raised Exception.
+    '''
+    with open('SPIN_REPORT.txt','a') as error_file :
+        traceback.print_exception(exc_type,exc_value,exc_traceback,file=error_file)
+
 def print_logo() -> None :
     '''
     Prints the software's logo, to use at the beginning.
     '''
     print("###############################################################")
     print(r" \    __________  ___",end=" ")
-    print("\033[91m___       __        _________   ___\033[00m",end=" ")
+    print("___       __        _________   ___",end=" ")
     print("   / ")
     print(r"  \   |*|  || \*\/*/ ",end=" ")
-    print("\033[91m|*|      /**"+r"\ "+"     /**___)|**"+r"\ "+"/**|\033[00m",end=" ")
+    print("|*|      /**"+r"\ "+"     /**___)|**"+r"\ "+"/**|",end=" ")
     print("  /  ")
     print(r"  /   |*|__||  \**/  ",end=" ")
-    print("\033[91m|*|__   /____"+r"\ "+"   |__**|  |*|*V*|*|\033[00m",end=" ")
+    print("|*|__   /____"+r"\ "+"   |__**|  |*|*V*|*|",end=" ")
     print(r"  \  ")
     print(r" /    |_|      |__|  ",end=" ")
-    print("\033[91m|____| /_/  "+r"\_"+r"\ "+"(_____/   |_|   |_|\033[00m",end=" ")
+    print("|____| /_/  "+r"\_"+r"\ "+"(_____/   |_|   |_|",end=" ")
     print(r"   \ ")
     print("###############################################################")
 
@@ -80,6 +97,8 @@ def solve_by_lanczos(hamiltonian: 'np.ndarray',n_excited: int,lanczos_digits: in
         n_excited (int): Number of excited manifolds to be computed;
         lanczos_digits (int): Number of Lanczos energy digits to be taken into account.
     '''
+    print('\nPerforming the Lanczos algorithm...')
+    
     # Ground-state degeneracy is initialized to 0 since unknown
     GS_deg = 0
     
@@ -103,6 +122,128 @@ def solve_by_lanczos(hamiltonian: 'np.ndarray',n_excited: int,lanczos_digits: in
         
         # Stop the loop if the Lanczos algorithm has estimated all the eigenstates of the highest-energy eigenspace
         if excited_count==n_excited+1 :
+            print('The Lanczos algorithm has converged successfully.')
             return states[:,:-1], energies[:-1], GS_deg
 
         n += 1
+
+def save_data(energies: 'np.ndarray',spin_correlations: tuple,magnetization: float) -> None :
+    '''
+    Saves all the relevant data about the just performed Lanczos aloìgorithm, as well as
+    the spin-spin correlation values and the magnetization.
+    All the items I have just mentioned are stored into a well-structured JSON file, 
+    called SPIN_OUT.json.
+    
+    Args:
+        energies (np.ndarray): Tuple containing the eigenenergies approximated by the Lanczos algorithm;
+        spin_correlations (tuple): Tuple of the spin-spin correlation matrices, the indices specify the associated spin pair;
+        magnetization (float): Modulus of the magnetization vector.
+    '''
+    print('\nSaving data...')
+    
+    # Initialize a dictionary collecting all the relevant data
+    data_dict = {
+        'Lanczos Energies': [float(np.real(e)) for e in energies.tolist()],
+        'Spin-Spin Corr. Matrices': {
+            'X components': {},
+            'Y components': {},
+            'Z components': {},
+            'Total': {}
+        },
+        'Magnetization Modulus': float(magnetization)
+    }
+    
+    # Fill in the spin-spin correlation values
+    for n in range(spin_correlations[0].shape[0]) :
+        for m in range(spin_correlations[0].shape[0]) :
+            i = 0
+            for key in data_dict['Spin-Spin Corr. Matrices'].keys() :
+                data_dict['Spin-Spin Corr. Matrices'][key][f'Spins {n}-{m}'] = spin_correlations[i][n][m]
+                i += 1
+    
+    # Write the data into a JSON file
+    with open('SPIN_OUT.json','w') as file :
+        json.dump(data_dict,file,indent=4)
+        print('All the relevant data is finally saved.')
+
+def map_spin_correlations(system: SpinSystem,spin_correlations: tuple,shell_digits: int,n_dim: int) -> tuple :
+    '''
+    Maps the just obtained spin-spin correlation values to the associated NN shell, exploiting the find_NN_shell
+    method defined above.
+    
+    Args:
+        spin_correlations (tuple): Tuple of the spin-spin correlation matrices, the indices specify the associated spin pair;
+        shell_digits (int): Number of digits to be considered during the identification of the NN shells by distance;
+        n_dim (int): Number of spatial dimensions of the spin system under study.
+    '''
+    # Target quantities
+    spin_corr_data = [[],[],[],[]]
+    distances = []
+    
+    # First element represents the "on-site" spin-spin correlation
+    for i in range(4) :
+        spin_corr_data[i].append(spin_correlations[i][0][0])
+    distances.append(0.0)
+    
+    # Take spin 0 as a reference and
+    # Loop over the NN shells until any replica of spin 0 is found
+    nn = 1
+    is_replica_found = False
+    while not is_replica_found :
+        shell_indices, shell_vectors = system.find_NN_shell(0,nn,shell_digits,n_dim)
+        
+        # Store the spin-spin correlation of the current NN shell
+        for i in range(4) :
+            spin_corr_data[i].append(spin_correlations[i][0][shell_indices[0]])
+        distances.append(np.linalg.norm(shell_vectors[0]))
+        
+        # Break the cycle just after any replica of spin 0 is found
+        if shell_indices.count(0)!=0 :
+            is_replica_found = True
+        
+        nn += 1
+    
+    return spin_corr_data, distances
+
+def plot_data(system: SpinSystem,spin_correlations: tuple,magnetization: float,shell_digits: int,n_dim: int) -> None :
+    '''
+    Use matplotlib tools for plotting the just obtained spin-spin correlation values as a function of the intersite distance
+    of the spin pair they are refered to. The resulting magnetization modulus is also reported as an horizontal line.
+      
+    Args:
+        system (SpinSystem): Physical system under study, in order to have an easier access to its properties;
+        spin_correlations (tuple): Tuple of the spin-spin correlation matrices, the indices specify the associated spin pair;
+        magnetization (float): Modulus of the magnetization vector;
+        shell_digits (int): Number of digits to be considered during the identification of the NN shells by distance;
+        n_dim (int): Number of spatial dimensions of the spin system under study.
+    '''
+    print('\nPlotting data...')
+    
+    # Set the default font family and size
+    plt.rcParams['font.family'] = 'Times New Roman'
+    plt.rcParams['font.size'] = 14
+
+    # Initialize the figure and add some aesthetics
+    fig, axs = plt.subplots(figsize=(10,6))
+    axs.set_title('Spin-spin correlation function',fontdict={'fontsize': 20})
+    axs.set_xlabel(r'Intersite distance |$\vec{R}_{ij}$| (Å)',fontdict={'fontsize': 16})
+    axs.set_ylabel('Exp. value',fontdict={'fontsize': 16})
+    styles = ['o-','s-','P-','d-']
+    labels = [r'$S^{(i)}_x \cdot S^{(j)}_x$',r'$S^{(i)}_y \cdot S^{(j)}_y$',r'$S^{(i)}_z \cdot S^{(j)}_z$',r'$\vec{S}^{(i)} \cdot \vec{S}^{(i)}$']
+    
+    # Sort the spin-spin correlations by the associated intersite distance
+    spin_corr_data, distances = map_spin_correlations(system,spin_correlations,shell_digits,n_dim)
+    x_min = -0.01*distances[-1]
+    x_max = 1.01*distances[-1]
+    axs.set_xlim(x_min,x_max)
+    for i in range(4) :
+        axs.plot(distances,spin_corr_data[i],styles[i],markersize=8,markerfacecolor='white',label=labels[i])
+    axs.plot(np.linspace(0.0,distances[-1],2),[magnetization for i in range(2)],'k--',label=r'|$\vec{M}$|')
+    
+    # Just some final aesthetics and save
+    plt.grid(linestyle='--',linewidth=0.5)
+    plt.fill_between(distances,spin_corr_data[3],color='gray',alpha=0.3)
+    plt.legend(loc='lower right')
+    plt.savefig('SPIN_CORRS.png')
+    print('All the relevant data is finally plotted.')
+    plt.show()
--- main.py
+++ main.py
@@ -1,42 +1,51 @@
-from global_functions import *
+from global_functions import log_exception, print_logo, solve_by_lanczos, save_data, plot_data
 from input_reader import InputReader
 from spin_system import SpinSystem
 
-import numpy as np
+import sys
 
 def main() :
-    # Print software logo
-    print_logo()
+    
+    with open('SPIN_REPORT.txt','w') as file :
+        sys.stdout = file
+        sys.excepthook = log_exception
+        
+        # Print software logo
+        print_logo()
 
-    # Read all input files
-    reader = InputReader()
-    reader.print_summary()
-    
-    # Initialize the spin system
-    lattice_vectors = reader.get_lattice_vectors()
-    mag_ions_pos = reader.get_mag_ions_pos()
-    spin = reader.get_spin()
-    n_dim = reader.get_n_dim()
-    system = SpinSystem(lattice_vectors,mag_ions_pos,spin)
-    
-    # Build the associated Hamiltonian matrix
-    J_couplings = reader.get_J_couplings()
-    T_vectors = reader.get_T_vectors()
-    max_NN_shell = reader.get_max_NN_shell()
-    shell_digits = reader.get_shell_digits()
-    B_field = reader.get_B_field()
-    tol_imag = reader.get_tol_imag()
-    hamiltonian = system.build_hamiltonian(J_couplings,T_vectors,max_NN_shell,shell_digits,B_field,n_dim,tol_imag)
-    
-    # Perform the Lanczos algorithm
-    n_excited = reader.get_n_excited()
-    lanczos_digits = reader.get_lanczos_digits()
-    states, energies, GS_deg = solve_by_lanczos(hamiltonian,n_excited,lanczos_digits)
-    
-    # Extract the requested output
-    magn_output_mode = reader.get_magn_output_mode()
-    spin_correlations = system.compute_all_spin_correlations(states,GS_deg)
-    magnetization = system.compute_magnetization(states,GS_deg,magn_output_mode)
+        # Read all input files
+        reader = InputReader()
+        reader.print_summary()
+        
+        # Initialize the spin system
+        lattice_vectors = reader.get_lattice_vectors()
+        mag_ions_pos = reader.get_mag_ions_pos()
+        spin = reader.get_spin()
+        n_dim = reader.get_n_dim()
+        system = SpinSystem(lattice_vectors,mag_ions_pos,spin)
+        
+        # Build the associated Hamiltonian matrix
+        J_couplings = reader.get_J_couplings()
+        T_vectors = reader.get_T_vectors()
+        max_NN_shell = reader.get_max_NN_shell()
+        shell_digits = reader.get_shell_digits()
+        B_field = reader.get_B_field()
+        tol_imag = reader.get_tol_imag()
+        hamiltonian = system.build_hamiltonian(J_couplings,T_vectors,max_NN_shell,shell_digits,B_field,n_dim,tol_imag)
+        
+        # Perform the Lanczos algorithm
+        n_excited = reader.get_n_excited()
+        lanczos_digits = reader.get_lanczos_digits()
+        states, energies, GS_deg = solve_by_lanczos(hamiltonian,n_excited,lanczos_digits)
+        
+        # Extract the requested output
+        magn_output_mode = reader.get_magn_output_mode()
+        spin_correlations = system.compute_all_spin_correlations(states,GS_deg)
+        magnetization = system.compute_magnetization(states,GS_deg,magn_output_mode)
+        
+        # Save and plot all the relevant data
+        save_data(energies,spin_correlations,magnetization)
+        plot_data(system,spin_correlations,magnetization,shell_digits,n_dim)
 
 if __name__ == '__main__':
     main()
